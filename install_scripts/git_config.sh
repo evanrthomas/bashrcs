feea2f1375a903c55a23225d3f363dbeb107c16a
Evan Thomas <evanrthom@gmail.com>
2015-02-14 13:32:28 -0500

    evan's changes

--- install_scripts/git_config.sh
+++ install_scripts/git_config.sh
@@ -1,6 +1,4 @@
-git config --global push.default upstream
 git config --global core.excludesfile ~/.gitignore_global
-git config --global branch.autosetupmerge true
 
 #Git editor/tool choices
 git config --global core.editor "vim"
@@ -8,22 +6,5 @@ git config merge.tool vimdiff
 git config merge.conflictstyle diff3
 git config diff.tool vimdiff
 
-#Git coloring
-git config --global color.ui true
-#---branch coloring
-git config --global color.branch.current "magenta"
-git config --global color.branch.local "yellow"
-git config --global color.branch.remote "green"
-#---diff coloring
-git config --global color.diff.meta "white"
-git config --global color.diff.frag "magenta bold"
-git config --global color.diff.old "red bold"
-git config --global color.diff.new "green bold"
-git config --global color.diff.whitespace "white reverse"
-#--- status coloring
-git config --global color.status.added "green"
-git config --global color.status.changed "yellow"
-git config --global color.status.untracked "cyan"
-git config --global color.status.branch "magenta"
 #---- Other options
 git config --global grep.linenumber true
